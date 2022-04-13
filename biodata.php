c026c406191edd4e9b102f06cd104ba961e3b7ed
I KADEK MAHA GANGGA <102144626+ikadekmahagangga@users.noreply.github.com>
2022-04-13 18:55:24 +0800

    Add files via upload

---
+++ biodata.php
@@ -0,0 +1,32 @@
+<!-- table.html -->
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>table</title>
+        </head>
+        <body>
+            <form name="form1" action="hasil_biodata.php">
+                <table width="400" border="1">
+                    <tr>
+                        <td colspan="2" width="400" align="center">INPUT DATA</td>
+                    </tr>
+                    <tr>
+                        <td width="200">nama</td>
+                        <td colspan="2" width="200"><input type="text" name="nama"></td>
+                    </tr>
+                    <tr>
+                        <td width="200">alamat</td>
+                        <td colspan="2" width="200"><input type="text" name="alamat"></td>
+                    </tr>
+                    <tr>
+                        <td width="200">Tanggal_lahir</td>
+                        <td colspan="2" width="200"><input type="text" name="tgl"></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" width="400" align="center"><input type="submit" value="PROSES "></td>
+
+                    </tr>
+                </table>
+            </form>
+        </body>
+    </html>
---
+++ hasil_biodata.php
@@ -0,0 +1,8 @@
+<?php
+	$nama = $_REQUEST['nama'];
+	$alamat = $_REQUEST['alamat'];
+	$tgl = $_REQUEST['tgl'];
+
+	echo "Hello $nama,</br> anda tinggal di $alamat"; 
+	echo "</br>kamu lahir $tgl";
+?>
